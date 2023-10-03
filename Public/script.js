e7afaabd02a3e6f0bb0f0fc8ab5cf4dcaedb3b74
Sheroz Akram <sheroz.akram@proton.me>
2023-10-03 11:45:06 +0500

    update account creation

--- Public/script.js
+++ Public/script.js
@@ -1,18 +1,46 @@
 // Toggle Between Sign In and Sign Up Pages by using a Integer
 let activeAccountBox = 0;
-function toggleAccountBox(){
-    if(activeAccountBox == 0){
+function toggleAccountBox(clicker){
+    if(activeAccountBox == 0 && clicker == 1){
         document.getElementById("loginSelectButton").className = "";
         document.getElementById("signUpSelectButton").className = "active"
         document.getElementById("signInBox").style.display = "none";
         document.getElementById("signUpBox").style.display = "block";
         activeAccountBox = 1;
     }
-    else{
+    else if(activeAccountBox == 1 && clicker == 0){
         document.getElementById("loginSelectButton").className = "active";
         document.getElementById("signUpSelectButton").className = "";
         document.getElementById("signInBox").style.display = "block";
         document.getElementById("signUpBox").style.display = "none";
         activeAccountBox = 0;
+    }
+}
+
+// Sign In a New User
+function signInUser(){
+
+    // Get the Form Data from the DOM
+    let userName = document.getElementById("userNameLogin").value;
+    let userPass = document.getElementById("userPassLogin").value;
+
+    // Create A Form Data for the User
+    let loginFormData = new FormData();
+    loginFormData.append("requestType", "signin");
+    loginFormData.append("username", userName);
+    loginFormData.append("password", userPass);
+
+    // Check If Both are Empty or Not
+    if(userName.length == 0 || userPass.length == 0){
+        window.alert("Not Complete")
+    }
+    // Send the Login Request to the Server
+    else{
+        fetch("/AccountAPI", {
+            method: "POST",
+            body: loginFormData
+        }).then(res => res.json()).then(data => {
+            console.log(data);
+        })
     }
 }
--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 // Import Our Libraries
 let express = require("express");
+let multer = require("multer");
 let path = require("path");
 let cors = require("cors");
 
@@ -7,12 +8,18 @@ let cors = require("cors");
 let clientDirectory = path.join(__dirname, "../Client/")
 let publicDirectory = path.join(__dirname, "../Public/")
 
+// Initialize Our Multer Form Praser
+let multerUpload = multer();
+
 // Create a Server and Configure
 let app = express();
 app.use(express.static(publicDirectory))
 app.use(cors({
     "origin": "*"
 }));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(multerUpload.none())
 
 // Make routes for Website
 
@@ -24,6 +31,32 @@ app.get("/", (req, res) => {
 // Chat Pages
 app.get("/Chat", (req, res) => {
     res.redirect("/Login");
+})
+
+// Create Account and Sign In New User
+app.post("/AccountAPI", (req, res) => {
+    console.log(req.body)
+
+    // Get the Type of Request
+    let requestType = req.body["requestType"];
+
+    // Login Implementation
+    if (requestType == "signin") {
+
+    }
+
+    // Sign Up a new User
+    else if (requestType == "signup") {
+
+    }
+
+    // In Case of Invalid Request
+    else {
+        res.send({
+            "ResponseCode": "404",
+            "Message": "Invalid API Endpoint"
+        })
+    }
 })
 
 // Terms and Conditions Page
